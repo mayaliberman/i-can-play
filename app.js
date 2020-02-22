5c0ba3bc589d6aba0dd35c2cff5e854a43c1bbf7
unknown <mayaliberman1@gmail.com>
2020-02-22 17:22:20 +0200

    start button eliminating keys

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-'use strict';
-const c = document.querySelector('.c');
+"use strict";
+const notes = document.querySelectorAll(".note");
+const c = document.querySelector(".c");
 const d = document.querySelector(".d");
 const e = document.querySelector(".e");
 const f = document.querySelector(".f");
@@ -7,7 +8,8 @@ const g = document.querySelector(".g");
 const a = document.querySelector(".a");
 const b = document.querySelector(".b");
 const highC = document.querySelector(".high-c");
-
+const startGameButton = document.querySelector(".start-game");
+const score = 0;
 const patterns = [
   [0, 1, 2, 0, 2, 1, 1, 0, 2, 1, 2, 0, 2, 0, 1, 2, 1, 0],
   [0, 1, 3, 0, 3, 1, 1, 0, 3, 1, 3, 0, 3, 0, 1, 3, 1, 0],
@@ -15,14 +17,21 @@ const patterns = [
   [0, 1, 5, 0, 5, 1, 1, 0, 5, 1, 5, 0, 5, 0, 1, 5, 1, 0],
   [0, 1, 6, 0, 6, 1, 1, 0, 6, 1, 6, 0, 6, 0, 1, 6, 1, 0],
   [0, 1, 7, 0, 7, 1, 1, 0, 7, 1, 7, 0, 7, 0, 1, 7, 1, 0]
-  
+];
+const keys = [
+  [0, 1, 2],
+  [0, 1, 3],
+  [0, 1, 4],
+  [0, 1, 5],
+  [0, 1, 6],
+  [0, 1, 7]
 ];
 
 c.onclick = () => {
-   const sound = new Audio(); // create the audio
-    sound.src = "sounds/xylophone-c.mp3";  
-    sound.play();
-}
+  const sound = new Audio(); // create the audio
+  sound.src = "sounds/xylophone-c.mp3";
+  sound.play();
+};
 d.onclick = () => {
   const sound = new Audio(); // create the audio
   sound.src = "sounds/xylophone-d1.mp3";
@@ -58,3 +67,12 @@ highC.onclick = () => {
   sound.src = "sounds/xylophone-c2.mp3";
   sound.play();
 };
+
+startGameButton.addEventListener("click", () => {
+//   document.querySelector(".desc").style.visibility = "hidden";
+  for (let i = 0; i < notes.length; i++) {
+    if (i !== keys[0][i]) {
+      notes[i].style.display = "none";
+    }
+  }
+});
