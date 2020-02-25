b1824d818c4974471453935b6dd6ca1fff2be442
unknown <mayaliberman1@gmail.com>
2020-02-25 18:17:17 +0200

    refactor with elad

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 const notes = document.querySelectorAll(".note");
+// TODO: Convert to function/loop;
 const c = document.querySelector(".c");
 const d = document.querySelector(".d");
 const e = document.querySelector(".e");
@@ -7,11 +8,14 @@ const f = document.querySelector(".f");
 const g = document.querySelector(".g");
 const a = document.querySelector(".a");
 const b = document.querySelector(".b");
-const highC = document.querySelector(".high-c");
+const c2 = document.querySelector(".c2");
 const startGameButton = document.querySelector(".start-game");
+const gameStatus = document.querySelector(".game-status");
 const xylophone = document.querySelector(".xylophone");
-const score = 0;
+let currentLevel = 0;
 
+let score = 0;
+let playerPattern = [];
 const patterns = [
   [0, 1, 2, 0, 2, 1, 1, 0, 2, 1, 2, 0, 2, 0, 1, 2, 1, 0],
   [0, 1, 3, 0, 3, 1, 1, 0, 3, 1, 3, 0, 3, 0, 1, 3, 1, 0],
@@ -20,69 +24,38 @@ const patterns = [
   [0, 1, 6, 0, 6, 1, 1, 0, 6, 1, 6, 0, 6, 0, 1, 6, 1, 0],
   [0, 1, 7, 0, 7, 1, 1, 0, 7, 1, 7, 0, 7, 0, 1, 7, 1, 0]
 ];
-const keys = [
-  [0, 1, 2],
-  [0, 1, 3],
-  [0, 1, 4],
-  [0, 1, 5],
-  [0, 1, 6],
-  [0, 1, 7]
-];
-const stages = patterns.length;
-const gameStatus = document.querySelector(".game-status");
+const levels = patterns.length;
+const keySounds = { c, d, e, f, g, a, b, c2 };
 
-c.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-c.mp3";
-  sound.play();
-};
-d.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-d1.mp3";
-  sound.play();
-};
-e.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-e1.mp3";
-  sound.play();
-};
-f.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-f.mp3";
-  sound.play();
-};
-g.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-g.mp3";
-  sound.play();
-};
-a.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-a.mp3";
-  sound.play();
-};
-b.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-b.mp3";
-  sound.play();
-};
-highC.onclick = () => {
-  const sound = new Audio(); // create the audio
-  sound.src = "sounds/xylophone-c2.mp3";
-  sound.play();
+// TODO: Convert hard coded keys to function
+const keysForCurrentLevel = currentLevel => {
+  const maxNum = Math.max(...patterns[currentLevel]);
+  const numOfKeys = [];
+  for (let i = 0; i <= maxNum; i++) {
+    numOfKeys.push(i);
+  }
+  return numOfKeys;
 };
 
 
+function addSounds(sounds) {
+ 
+  for (let sound in sounds) {
+    sounds[sound].onclick = () => {
+      const mp3 = new Audio();
+      mp3.src = `sounds/xylophone-${sound}.mp3`;
+      // console.log(mp3.src)
+      mp3.play();
+    };
+  }
+}
+addSounds(keySounds);
 
-
-
-
+// TODO: Convert for loop to filter on keys array
 startGameButton.addEventListener("click", () => {
-    xylophone.style.display = 'flex';
-    gameStatus.style.display = 'none';
-    
+  const keys = keysForCurrentLevel(currentLevel);
   for (let i = 0; i < notes.length; i++) {
-    if (i !== keys[0][i]) {
+    if (i !== keys[i]) {
       notes[i].style.display = "none";
     }
   }
@@ -91,28 +64,36 @@ startGameButton.addEventListener("click", () => {
 });
 
 function checkWin() {
-  let playerPattern = [];
-  
   xylophone.addEventListener("click", e => {
-    playerPattern.push(parseInt(e.target.dataset["key"]));
+    if (e.target.tagName === "BUTTON") {
+      playerPattern.push(parseInt(e.target.dataset["key"]));
 
-    if (playerPattern.length === 18) {
-     
-      for (let i = 0; i < playerPattern.length; i++) {
-        if (playerPattern[i] !== patterns[0][i]) {
-          xylophone.style.display = "none";
-          gameStatus.textContent = "Nice Try!";
+      if (playerPattern.length === 18) {
+        for (let i = 0; i < playerPattern.length; i++) {
+          if (playerPattern[i] !== patterns[currentLevel][i]) {
+            xylophone.style.display = "none";
+            gameStatus.textContent = "Nice Try!";
             gameStatus.style.display = "block";
             startGameButton.style.display = "block";
-            playerPattern = [];
-          return;
-        } else {
-          xylophone.style.display = "none";
+          } else {
+            xylophone.style.display = "none";
             gameStatus.style.display = "block";
             startGameButton.style.display = "block";
-            playerPattern = [];
+            level++;
+          }
         }
       }
     }
   });
+}
+
+function nextLevel() {
+  level++;
+  playerPattern = [];
+}
+
+function restartGame() {
+  xylophone.style.display = "flex";
+  gameStatus.style.display = "none";
+  playerPattern = [];
 }
